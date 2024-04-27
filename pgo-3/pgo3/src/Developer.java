d204aa9ea495a74b02ae6c1996dfabf044339671
MateuszSwiech <163740794+MateuszSwiech@users.noreply.github.com>
2024-04-27 02:16:53 +0200

    Exercise 3

--- pgo-3/pgo3/src/Developer.java
+++ pgo-3/pgo3/src/Developer.java
@@ -1,2 +1,22 @@
-public class Developer {
+import java.util.ArrayList;
+
+public class Developer extends Employee {
+    private ArrayList<Technology> technologies;
+
+    public Developer(String fname, String lname, String address, String email, String pesel, int EmploymentYear) {
+        super(fname, lname, address, email, pesel, EmploymentYear);
+        this.technologies = new ArrayList<>();
+    }
+
+    public void addTechnology(Technology technology) {
+        technologies.add(technology);
+    }
+
+    public int calculateSalary() {
+        int baseSalary = super.calculateSalary();
+        for (Technology technology : technologies) {
+            baseSalary += technology.getBonus();
+        }
+        return baseSalary;
+    }
 }
