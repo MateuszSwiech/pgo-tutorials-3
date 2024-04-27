fd2c7dd484cdde4556466fa0f39fc6f081a767cd
MateuszSwiech <163740794+MateuszSwiech@users.noreply.github.com>
2024-04-27 02:16:05 +0200

    Exercise 3

--- pgo-3/pgo3/src/Goal.java
+++ pgo-3/pgo3/src/Goal.java
@@ -1,2 +1,24 @@
 public class Goal {
+    private int year;
+    private int month;
+    private int day;
+    private String name;
+    private int bonus;
+
+    public Goal(int year, int month, int day, String name, int bonus) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public boolean isAchieved(int currentYear, int currentMonth) {
+        return this.year == currentYear && this.month == currentMonth;
+    }
+
+    public int getBonus() {
+
+        return bonus;
+    }
 }
