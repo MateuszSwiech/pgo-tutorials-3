3104f8df00843b34a69a291862aec019df45bfea
MateuszSwiech <163740794+MateuszSwiech@users.noreply.github.com>
2024-04-27 02:22:31 +0200

    Exercise 3

--- pgo-3/pgo3/src/Tester.java
+++ pgo-3/pgo3/src/Tester.java
@@ -1,2 +1,21 @@
-public class Tester {
+import java.util.ArrayList;
+
+public class Tester extends Employee {
+    private ArrayList<String> testTypes;
+
+    public Tester(String fname, String lname, String address, String email, String pesel, int EmploymentYear) {
+        super(fname, lname, address, email, pesel, EmploymentYear);
+        this.testTypes = new ArrayList<>();
+    }
+
+    public void addTestType(String testType) {
+        testTypes.add(testType);
+    }
+
+    @Override
+    public int calculateSalary() {
+        int baseSalary = super.calculateSalary();
+        baseSalary += testTypes.size() * 300;
+        return baseSalary;
+    }
 }
